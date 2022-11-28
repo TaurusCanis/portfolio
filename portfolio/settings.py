93cc23541158f4c7ececdd8a63d6a45a0a05894b
Taurus Canis <acdole09@gmail.com>
2022-11-28 16:45:40 -0500

    fixed DEBUG issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ AUTH_USER_MODEL = 'tutor_tracker_backend.User'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = os.environ.get('DEBUG')
 PRODUCTION = os.getenv("PRODUCTION", True) 
 
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(",") if PRODUCTION else ['*']
