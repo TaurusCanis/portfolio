0c9b43a8a6659257f7f0037604549504cfa781bc
Taurus Canis <acdole09@gmail.com>
2022-11-28 17:18:30 -0500

    commented out default permissions settings for DRF

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -177,8 +177,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated', )
 }
 
 EMAIL_HOST = 'smtp.gmail.com' 
