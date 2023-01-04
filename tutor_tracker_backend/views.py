d294c30d0122ef64c94f423fb76fdc7eaeb2af81
Taurus Canis <acdole09@gmail.com>
2023-01-04 15:32:57 -0500

    adding print statement for debugging in TuttorTrackerUserViewSet

--- tutor_tracker_backend/views.py
+++ tutor_tracker_backend/views.py
@@ -5,26 +5,27 @@ from rest_framework.decorators import permission_classes, action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .serializers import (
-    UserSerializer, CustomerSerializer, AppointmentSerializer, PaymentSerializer,
-    AppointmentNoteSerializer
+    CustomerSerializer, AppointmentSerializer, PaymentSerializer,
+    AppointmentNoteSerializer, TutorTrackerUserSerializer, InvoiceSerializer,
+    StatementSerializer, PrepaymentSerializer
 )
 from .models import (
-    Customer, Appointment, Payment, AppointmentNote
+    Customer, Appointment, Payment, AppointmentNote, TutorTrackerUser, Invoice,
+    Statement, Prepayment
 )
-from django.db.models import F
+from django.db.models import F, Sum, Q
 from decimal import Decimal
 from django.core.mail import send_mail
 import datetime
+from django.conf import settings
+
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list` and `retrieve` actions.
-    """
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+class TutorTrackerUserViewSet(viewsets.ModelViewSet):
+    serializer_class = TutorTrackerUserSerializer
+    queryset = TutorTrackerUser.objects.all()
     permission_classes_by_action = {
         "default": [IsAuthenticated],
         "create": [AllowAny],
@@ -44,8 +45,29 @@ class UserViewSet(viewsets.ModelViewSet):
                 for permission in self.permission_classes_by_action["default"]
             ]
 
+    # Create a new User using AUTH_USER_MODEL
+    def create_user(self):
+        return User.objects.create_user(**self.request.data)
+
+    # Create a new TutorTrackerUser
+    def create(self, request, *args, **kwargs):
+        user = self.create_user()
+        data = self.request.data
+        data['user'] = user.id
+        serializer = self.get_serializer(data=data)
+        print("serializer.is_valid(): ", serializer.is_valid())
+        print("serializer.errors: ", serializer.errors)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer, user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # Save relationship to new AUTH_USER_MODEL instance
+    def perform_create(self, serializer, user):
+        serializer.save(user=user)
+
     def get_object(self):
-        return self.request.user
+        return TutorTrackerUser.objects.get(user=self.request.user)
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """
@@ -55,17 +77,18 @@ class CustomerViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.request.user.customers.filter(**self.request.query_params.dict()).order_by('last_name')
+        return Customer.objects.filter(user__user=self.request.user, **self.request.query_params.dict()).order_by('last_name')
+        # return self.request.user.tutor_tracker_user.customers.filter(**self.request.query_params.dict()).order_by('last_name')
 
     def create(self, serializer):
         return super().create(serializer)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user.tutor_tracker_user.first())
 
     def update(self, request, pk=None):
         obj = self.get_object()
-        if obj.user.id == self.request.user.id:
+        if obj.user.user.id == self.request.user.id:
             return super().update(request)
         return Response(status=status.HTTP_400_BAD_REQUEST) # How to return Error in DRF?
 
@@ -93,7 +116,8 @@ class AppointmentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         query_args = { self.format_key(key, value): self.format_value(value) for key, value in self.request.query_params.items() } 
-        return Appointment.objects.filter(customer__user=self.request.user, **query_args).order_by('date_time')
+        print("QUERY ARGS: ", query_args)
+        return Appointment.objects.filter(customer__user__user=self.request.user, **query_args).order_by('date_time')
 
     def get_customer(self, customer_id):
         customer = Customer.objects.get(id=customer_id)
@@ -140,37 +164,62 @@ class AppointmentViewSet(viewsets.ModelViewSet):
         customer.current_balance = F('current_balance') + fee_delta
         customer.save()
 
+    @action(detail=False, methods=['post'])
+    def filter_appointments(self, request, *args, **kwargs):
+        print(request.data)
+        filter_args = { key: value for key, value in request.data.items() if value != None}
+        if "start_date" in filter_args:
+            start_date = filter_args.pop("start_date")
+            filter_args['date_time__gte'] = start_date
+        if "end_date" in filter_args:
+            end_date = filter_args.pop("end_date")
+            filter_args['date_time__lte'] = end_date
+        if "appointment_notes__status" in filter_args:
+            if filter_args['appointment_notes__status'] == 'X':
+                value = filter_args.pop("appointment_notes__status")
+                filter_args[self.format_key('appointment_notes__status', value)] = self.format_value(None)
+        return Response(AppointmentSerializer(Appointment.objects.filter(**filter_args), many=True).data)
+
 class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Payment.objects.filter(customer__user=self.request.user).order_by('date') 
+        return Payment.objects.filter(customer__user__user=self.request.user).order_by('date') 
 
     def get_customer(self, customer_id):
         customer = Customer.objects.get(id=customer_id)
         return customer
 
     def perform_create(self, serializer):
-        payment_delta = -Decimal(self.request.data.get('amount'))
+        amount = Decimal(self.request.data.get('amount'))
+        payment_delta = -amount
         customer_id = self.request.data.get('customer')
-        self.update_customer_balance(payment_delta, customer_id)
+        self.update_customer_balance(payment_delta, customer_id, self.request.data.get('source'))
         serializer.save()
 
     def perform_update(self, serializer):
         payment_delta = Decimal(serializer.instance.amount) - Decimal(self.request.data.get('amount'))
         customer_id = self.request.data.get('customer')
-        self.update_customer_balance(payment_delta, customer_id)
+        self.update_customer_balance(payment_delta, customer_id, serializer.instance.source)
         serializer.save() 
 
     def perform_destroy(self, instance):
         payment_delta = instance.amount
-        self.update_customer_balance(payment_delta, instance.customer.id)
+        self.update_customer_balance(payment_delta, instance.customer.id, instance.source)
         super().perform_destroy(instance)
 
-    def update_customer_balance(self, payment_delta, customer_id):
+    def update_customer_balance(self, payment_delta, customer_id, payment_source):
         customer = self.get_customer(customer_id)
-        customer.current_balance = F('current_balance') + payment_delta
+        new_prepayment_balance = F('prepayment_balance') + payment_delta
+        new_current_balance = F('current_balance') + payment_delta
+        if payment_source == 'PB':
+            if new_prepayment_balance < 0:
+                new_current_balance += new_prepayment_balance
+                new_prepayment_balance = 0
+            else:
+                customer.prepayment_balance = new_prepayment_balance
+        customer.current_balance = new_current_balance
         customer.save()
 
 class AppointmentNoteViewSet(viewsets.ModelViewSet):
@@ -186,7 +235,7 @@ class AppointmentNoteViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_queryset(self):
-        return AppointmentNote.objects.filter(appointment__customer__user=self.request.user)
+        return AppointmentNote.objects.filter(appointment__customer__user__user=self.request.user)
 
     def get_appointment(self, kwargs):
         return Appointment.objects.get(**kwargs)
@@ -215,7 +264,150 @@ class AppointmentNoteViewSet(viewsets.ModelViewSet):
 
     def update_appointment_note_details(self):
         appointment_note = self.get_object()
-        appointment_note.status='C'
+        appointment_note.status='S'
         appointment_note.date_sent=datetime.datetime.now()
         appointment_note.save()
         return
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    serializer_class = InvoiceSerializer 
+    permission_classes = [permissions.IsAuthenticated]
+  
+    def get_queryset(self):
+        return Invoice.objects.filter(appointment__customer__user__user=self.request.user)
+
+    def update_invoice_details(self):
+        invoice = self.get_object()
+        invoice.sent=True
+        invoice.save()
+        return
+
+    @action(detail=True, methods=['post'])
+    def send_invoice_email(self, request, *args, **kwargs):
+        invoice = self.get_object()
+        try:
+            send_mail(
+                f'Invoice - {invoice.__str__()}',
+                f'{invoice.appointment.customer}\n'
+                f'{invoice.date}\n'
+                f'{invoice.amount}',
+                'tauruscanisrex@gmail.com',
+                [f'{invoice.appointment.customer.email_primary}'],
+                fail_silently=False,
+            )
+            self.update_invoice_details()
+            return Response()
+        except Exception as e:
+            print("Email failed to send: ", e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class StatementViewSet(viewsets.ModelViewSet):
+    serializer_class = StatementSerializer 
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_customer(self):
+        return Customer.objects.get(id=self.request.data.get('customer'))
+
+    def get_appointments(self, customer):
+        return Appointment.objects.filter(
+            Q(status='C') | Q(status='X-C'),
+            customer=customer, 
+            date_time__gte=self.request.data.get('start_date'),
+            date_time__lte=self.request.data.get('end_date'),
+        )
+
+    def get_payments(self, customer):
+        return Payment.objects.filter(
+            customer=customer,  
+            date__gte=self.request.data.get('start_date'),
+            date__lte=self.request.data.get('end_date')
+        )
+
+    def perform_create(self, serializer):
+        statement_data = self.get_statement_data()
+        serializer.save(
+            appointments=statement_data['appointments'],
+            payments=statement_data['payments'],
+            incurred_charges=statement_data['incurred_charges'],
+            balance_due=statement_data['balance_due'],
+            previous_balance=statement_data['previous_balance']
+        )
+  
+    def get_queryset(self):
+        return Statement.objects.filter(customer__user__user=self.request.user)
+
+    def get_statement_data(self):
+        customer = self.get_customer()
+        appointments = self.get_appointments(customer)
+        payments = self.get_payments(customer)
+        incurred_charges = self.get_incurred_charges(appointments)
+        previous_balance = self.get_previous_balance(customer)
+        total_payments = self.get_total_payments(payments)
+        balance_due = self.get_balance_due(previous_balance, incurred_charges, total_payments)
+        return {
+            "appointments": appointments,
+            "payments": payments,
+            "incurred_charges": incurred_charges,
+            "balance_due": balance_due,
+            "previous_balance": previous_balance
+        }
+
+    def update_statement_details(self):
+        statement = self.get_object()
+        statement.sent=True
+        statement.save()
+        return
+
+    def get_incurred_charges(self, appointments):
+        return appointments.aggregate(total_fees=Sum('fee'))['total_fees'] or 0
+
+    def get_total_payments(self, payments):
+        return payments.aggregate(total_payments=Sum('amount'))['total_payments'] or 0.00
+
+    def get_balance_due(self, previous_balance, incurred_charges, total_payments):
+        return Decimal(previous_balance) + Decimal(incurred_charges) - Decimal(total_payments) or 0
+
+    def get_previous_balance(self, customer):
+        try:
+            return customer.statements.order_by("-end_date").first().balance_due
+        except:
+            return 0
+
+    @action(detail=True, methods=['post'])
+    def send_statement_email(self, request, *args, **kwargs):
+        statement = self.get_object()
+        try:
+            send_mail(
+                f'Statement - {statement.__str__()}',
+                f'{statement.customer}\n'
+                f'{statement.start_date} - {statement.end_date}\n'
+                f'{statement.balance_due}',
+                'tauruscanisrex@gmail.com',
+                [f'{statement.customer.email_primary}'],
+                fail_silently=False,
+            )
+            self.update_statement_details()
+            return Response()
+        except Exception as e:
+            print("Email failed to send: ", e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class PrepaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PrepaymentSerializer 
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Prepayment.objects.filter(customer__user__user=self.request.user)
+
+    def get_customer(self, customer_id):
+        return Customer.objects.get(id=customer_id)
+
+    def perform_create(self, serializer):
+        print("SERIALIZER: ", serializer.validated_data)
+        self.update_customer(serializer.validated_data)
+        serializer.save()
+
+    def update_customer(self, serializer):
+        customer = self.get_customer(serializer['customer'].id)
+        customer.prepayment_balance += Decimal(serializer['amount'])
+        customer.save()
